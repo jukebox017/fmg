4e06b00997b02395db6d11e99dbb7e35157e1cbc
Daniel Lineberger <ace017@gmail.com>
2022-06-14 00:42:11 -0600

    Dockerize

--- api/server.ts
+++ api/server.ts
@@ -1,15 +1,26 @@
-var { graphql, buildSchema } = require('graphql');
+let express = require('express');
+let { graphqlHTTP } = require('express-graphql');
+let { buildSchema } = require('graphql');
 
-var schema = buildSchema(`
+// Construct a schema, using GraphQL schema language
+let schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
 
-var rootValue = { hello: () => 'Hello world!' };
+// The root provides a resolver function for each API endpoint
+let root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
-var source = '{ hello }';
-
-graphql({ schema, source, rootValue }).then((response) => {
-  console.log(response);
-});
+let app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
--- frontend.dockerfile
+++
@@ -1,7 +0,0 @@
-# Use small docker image
-FROM node:slim
-WORKDIR /app
-COPY web/package.json
-RUN npm install
-COPY . .
-EXPOSE 3000
